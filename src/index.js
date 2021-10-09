073c1b18d6a28019fc1f3e8d8d0129c07f52be81
Paulo <pauloh16.pdr@gmail.com>
2021-10-08 22:19:49 -0300

    Adicionar tipos escalares

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import { ApolloServer, gql } from 'apollo-server';
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
-      hello: String
-      hi: String
+      # Sinal de exclamação define um valor que não pode ser null.
+      id: ID!
+      name: String
+      age: Int
+      height: Float
+      married: Boolean
+      # Exclamação de dentro define que nenhum valor dentro do Array pode ser null.
+      nicknames: [String!]!
     }
   `,
   resolvers: {
     Query: {
-      hello: () => 'Hello World!',
-      hi: () => 'Hi World!',
+      id: () => '1',
+      name: () => 'Paulo Henrique',
+      age: () => 20,
+      height: () => 1.75,
+      married: () => false,
+      nicknames: () => ['Paulão', 'Rick', 'Henriqueta'],
     },
   },
 });

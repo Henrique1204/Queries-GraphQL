4e21075b9db6a78bc2d0a7043ed256fe3ca2059f
Paulo <pauloh16.pdr@gmail.com>
2021-10-08 22:00:22 -0300

    Adicionar primeiros resolvers

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       hello: String
+      hi: String
     }
   `,
+  resolvers: {
+    Query: {
+      hello: () => 'Hello World!',
+      hi: () => 'Hi World!',
+    },
+  },
 });
 
 server.listen(4003).then(({ url }) => console.log(`Rodando na url: ${url}`));
